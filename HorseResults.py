939e8952fc38b4bb27f3764453f3da7f8c264d17
dYoshitakaKato <phoenix1331019@gmail.com>
2021-12-30 11:56:14 +0000

    importの修正

--- HorseResults.py
+++ HorseResults.py
@@ -1,3 +1,19 @@
+import pandas as pd
+import numpy as np
+import datetime
+from tqdm.notebook import tqdm
+from sklearn.metrics import roc_auc_score
+from sklearn.preprocessing import LabelEncoder
+import lightgbm as lgb
+import requests
+from bs4 import BeautifulSoup
+import time
+from tqdm.notebook import tqdm
+import re
+from urllib.request import urlopen
+import optuna.integration.lightgbm as lgb_o
+from itertools import combinations, permutations
+
 #馬の過去成績データを処理するクラス
 class HorseResults:
     @staticmethod
--- Results.py
+++ Results.py
@@ -1,3 +1,20 @@
+import pandas as pd
+import numpy as np
+import datetime
+from tqdm.notebook import tqdm
+from sklearn.metrics import roc_auc_score
+from sklearn.preprocessing import LabelEncoder
+import lightgbm as lgb
+import requests
+from bs4 import BeautifulSoup
+import time
+from tqdm.notebook import tqdm
+import re
+from urllib.request import urlopen
+import optuna.integration.lightgbm as lgb_o
+from itertools import combinations, permutations
+
+
 class Results:
     @staticmethod
     def scrape(race_id_list):
